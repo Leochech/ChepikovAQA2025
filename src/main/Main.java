bd2e321522ba7f7dabdbe15ffdabf88e9169fe8e
Lev Chepikov <l.chepikov@bftcom.com>
2025-03-24 15:17:43 +0300

    fix: всё снёс

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,8 @@
 package src.main;
 
-import org.junit.Test;
-
 public class Main {
     public static void main(String[] args) {
         Variables variables = new Variables("Я переменная 1", "Я переменная 2");
         System.out.println(Variables.var3 + "! " + variables.var1 + "! " + variables.var2 + "! ");
-    }
-
-    @Test
-    public void tasksModuleFour() {
-        Car car1 = new Car("Toyota Rav4", 2022, "Бип-Бип!\n");
-        Car car2 = new Car("Volkswagen Polo", 2021, "Вов-вов!\n");
-        Car car3 = new Car("BMW X45", 2025, "Не включаю поворотник!");
-        car1.makeBebe();
-        car2.makeBebe();
-        car3.makeBebe();
     }
 }
