f042e6722895811fc4e8243cd4fb2d57f8dd6e37
Lev Chepikov <l.chepikov@bftcom.com>
2025-04-20 13:44:56 +0300

    feat: Выполнил задание модуля 9

--- src/main/HiByeHyperboleArr.java
+++ src/main/HiByeHyperboleArr.java
@@ -1,8 +1,10 @@
 package src.main;
 
+import java.util.Random;
+
 public class HiByeHyperboleArr {
 
-    static String[] array = {"привет","ПОКА","гипербола"};
+    static String[] array = {"привет", "ПОКА", "гипербола"};
 
 
     public HiByeHyperboleArr outputFirstStringInArray() {
@@ -17,12 +19,26 @@ public class HiByeHyperboleArr {
 
     public HiByeHyperboleArr fiendWordWhereSecondO() {
         for (String word : array) {
-            if(word.length()>=4 && word.substring(1,2).equalsIgnoreCase("о")) {
-                String threeAndFourCharacter = word.substring(0,3) + " " + word.substring(3);
+            if (word.length() >= 4 && word.substring(1, 2).equalsIgnoreCase("о")) {
+                String threeAndFourCharacter = word.substring(0, 3) + " " + word.substring(3);
                 System.out.println(threeAndFourCharacter);
                 break;
             }
         }
+        return this;
+    }
+
+    public HiByeHyperboleArr generateRandomNumber() {
+        int randomNumber = (int) (Math.random() * 3);
+        System.out.println(randomNumber);
+        return this;
+    }
+
+    public HiByeHyperboleArr getRandomString() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(array.length);
+        String randomString = array[randomIndex];
+        System.out.println(randomString);
         return this;
     }
 
